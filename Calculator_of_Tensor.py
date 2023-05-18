c21a5564f629f939b62f48e91331576c4eecb522
Yaroslem15 <110384995+Yaroslem15@users.noreply.github.com>
2023-05-18 17:47:16 +0300

    Update Calculator_of_Tensor.py

--- Calculator_of_Tensor.py
+++ Calculator_of_Tensor.py
@@ -1,9 +1,9 @@
 import os
+from parser_t import *
 
 
 
-
-match(int(input("Выберете действие: \n 1 - Умножение тензоров \n 2 - Нахождение тензора в новом базисе \n 3 - Симметрирование \n 4 - Альтернирование \n"))):
+match(int(input("Выберете действие: \n 1 - Умножение тензоров \n 2 - Нахождение тензора в новом базисе \n 3 - Симметрирование \n 4 - Альтернирование \n q - Для выхода из программы \n"))):
     case 1:
         os.system('python3 multiply.py')
     case 2:
@@ -12,3 +12,9 @@ match(int(input("Выберете действие: \n 1 - Умножение т
         os.system('python3 sym.py')
     case 4:
         os.system('python3 asym.py')
+    case ("q"):
+        os.system("clear")
+        anim()
+        os.system("exit")
+        
+os.system("python3 Calculator_of_Tensor.py")
